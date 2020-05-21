829d09e73d5f61dff2355439a65995d6b96af2f5
Pedro Zunino <peter@rootstrap.com>
2020-05-21 20:53:01 -0300

    Add loader

--- src/pages/PlayerPage.js
+++ src/pages/PlayerPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Loader from 'react-loaders'
+import 'loaders.css/src/animations/pacman.scss'
 
 import { getPlayer } from 'state/actions/playerActions'
 import { useDispatchOnMount } from 'hooks'
@@ -17,6 +19,14 @@ const PlayerPage = () => {
 
   return (
     <div className="container pb-4 mb-4">
+      {!player && (
+        <div className="center vh-100">
+          <div className="bg-dark rounded p-5">
+            <Loader type="pacman" />
+          </div>
+        </div>
+      )}
+      {!player && <Loader type="pacman" />}
       {player && <Profile image={player.avatarUrl} name={player.gamertag} />}
       {winRateByMap && <RateByMap rateByMap={winRateByMap} />}
     </div>
